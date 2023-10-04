5cce7a088b532cec9ccfb55c6ba2546c9e582d61
Harry <harry@rooit.me>
2023-10-04 19:03:13 +0900

    Initial Commit

---
+++ WAmazingTestSwiftUI/WAmazingTestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAmazingTestSwiftUIApp.swift
+//  WAmazingTestSwiftUI
+//
+//  Created by Harry on 2023/10/4.
+//
+
+import SwiftUI
+
+@main
+struct WAmazingTestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
